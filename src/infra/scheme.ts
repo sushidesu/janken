29be7dba8faa6a990b41bb81dfb7e913f99930c5
sushidesu <hugemerge@gmail.com>
2021-07-11 14:04:57 +0900

    データベースのschemeを定義

---
+++ src/infra/scheme.ts
@@ -0,0 +1,35 @@
+/*
+(root)
+  - rooms
+    - [roomID]: Room | undefined
+      - hands
+        - [id]: HandHistory | undefined
+          - hand: HandString
+          - userId: string
+      - host: User | undefined
+      - guest: User | undefined
+*/
+
+export const ROOM_ROOT_NAME = "rooms";
+export const ROOM_PATH = (roomId: string): string =>
+  `${ROOM_ROOT_NAME}/${roomId}`;
+export const HAND_ROOT_NAME = "hands";
+export const HAND_ROOT_PATH = (roomId: string): string =>
+  `${ROOM_PATH(roomId)}/${HAND_ROOT_NAME}`;
+
+export type KeyValue<T> = {
+  [id: string]: T;
+};
+
+export type Room = {
+  hands?: KeyValue<HandHistory>;
+  hostUserId: string | undefined;
+  hostUserName: string | undefined;
+  guestUserId?: string;
+  guestUserName?: string;
+};
+
+export type HandHistory = {
+  userId: string;
+  hand: string;
+};
